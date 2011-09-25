a380783b12b0892a0238e8bf7f83d8303d8e49ae
Ivari Horm <ivari@baltnet.ee>
2011-09-25 15:58:47 +0300

    Fixed IE display

--- www/index.php
+++ www/index.php
@@ -24,7 +24,7 @@ if(!$fsh->exists($url) || (defined("CACHE_FOLDER") && basename($url) == CACHE_FO
 
 
 ?>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
 		<title>Gallery</title>
