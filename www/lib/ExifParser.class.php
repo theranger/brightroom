0e36459f87e5f4b6ab501398396c6c30e884501d
The Ranger <ranger@risk.ee>
2011-10-08 15:17:49 +0300

    Fixed unknown array index notices.

--- www/lib/ExifParser.class.php
+++ www/lib/ExifParser.class.php
@@ -9,18 +9,26 @@ class ExifParser {
 	}
 	
 	public function getFileSize() {
+		if(!isset($this->exifData["FileSize"])) return;
+		 
 		return $this->exifData["FileSize"];
 	}
 	
 	public function getComment() {
+		if(!isset($this->exifData["Comments"])) return;
+		
 		return $this->exifData["Comments"];
 	}
 	
 	public function getTitle() {
+		if(!isset($this->exifData["Title"])) return;
+		
 		return $this->exifData["Title"];
 	}
 	
 	public function getDescription() {
+		if(!isset($this->exifData["Subject"])) return;
+		
 		return $this->exifData["Subject"];
 	}
 }
