59a178f3e95867ed4e7d005ddfeb487d5bcb1fac
The Ranger <ranger@risk.ee>
2012-06-18 00:40:37 +0300

    Added print commands

--- lib/ExifParser.class.php
+++ lib/ExifParser.class.php
@@ -3,9 +3,11 @@
 class ExifParser {
 	
 	private $exifData;
+	private $fileName;
 	
 	public function __construct($img) {
 		$this->exifData = exif_read_data($img);
+		$this->fileName = basename($img);
 	}
 	
 	public function getFileSize() {
@@ -14,21 +16,37 @@ class ExifParser {
 		return $this->exifData["FileSize"];
 	}
 	
+	public function printFileSize() {
+		echo $this->getFileSize();
+	}
+	
 	public function getComment() {
 		if(!isset($this->exifData["Comments"])) return;
 		
 		return $this->exifData["Comments"];
 	}
 	
+	public function printComment() {
+		echo $this->getComment();
+	}
+	
 	public function getTitle() {
-		if(!isset($this->exifData["Title"])) return;
+		if(!isset($this->exifData["Title"])) return $this->fileName;
 		
 		return $this->exifData["Title"];
+	}
+	
+	public function printTitle() {
+		echo $this->getTitle();
 	}
 	
 	public function getDescription() {
 		if(!isset($this->exifData["Subject"])) return;
 		
 		return $this->exifData["Subject"];
+	}
+	
+	public function printDescription() {
+		echo $this->getDescription();
 	}
 }
