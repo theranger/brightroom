03150729bb631ae40613996e7a4957a10ba31f44
The Ranger <ranger@risk.ee>
2013-04-06 14:33:44 +0300

    Added general view authorizaton.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include("lib/index.php");
 
+//Check if we have the permission to view URL
+if(!$session->authorize($urlParser->getDirectory())) {
+	print 'You don\'t have permission to view this object';
+	return;
+}
+
 //Load layout for displaying single image
 if($layout->isImage()) {
 	$f = "themes/".$layout->getTheme()."/single.php";
