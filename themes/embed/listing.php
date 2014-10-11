59d5484b95ac1f0785cc67a4cdc52b2ab40bda30
The Ranger <ranger@risk.ee>
2014-10-11 16:06:02 +0300

    Updated styling and layout

--- themes/embed/listing.php
+++ themes/embed/listing.php
@@ -1,11 +1,9 @@
 <!--  Begin Folder Gallery Section -->
 
 <link rel="stylesheet" type="text/css" href="<?php $layout->printThemeURL(); ?>/style.css" />
-<div class="sfg-section">
-	<div class="sfg-main">
-		<div class="sfg-head"><?php $layout->printBreadcrumb(); ?></div>
-		<?php $layout->printFolderContents(); ?>
-	</div>
+<div class="sfg-main">
+	<div class="sfg-head"><?php $layout->printBreadcrumb(); ?></div>
+	<?php $layout->printFolderContents(); ?>
 </div>
 
 <!--  End Folder Gallery Section -->
--- themes/embed/single.php
+++ themes/embed/single.php
@@ -5,17 +5,18 @@
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="<?php $layout->printThemeURL(); ?>/default.js"></script>
 
-<div class="sfg-section">
+<div class="sfg-nav"><?php $layout->printFolderContents(false); ?></div>
+
 <?php endif; ?>
 	<div class="sfg-main">
-		<div class="sfg-head"><?php $layout->printBreadcrumb(); ?></div>
-		<div class="sfg-content">
+		<div class="sfg-container">
+			<div class="sfg-breadcrumb"><?php $layout->printBreadcrumb(); ?></div>
+			<a class="sfg-back" href="<?php $layout->printDirectoryURL(); ?>">X</a>
 			<?php $layout->getImage(1024); ?>
 		</div>
 	</div>
 
 <?php if(!isset($_GET["sfg-ajax"])) :?>
-</div>
 
 <!--  End Folder Gallery Section -->
 <?php endif; ?>
