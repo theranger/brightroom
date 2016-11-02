e1bac6eb2bd072e317114623daea70f15cffed96
The Ranger <ranger@risk.ee>
2016-11-02 20:59:09 +0200

    Ensure working directory is correct during response composition

--- index.php
+++ index.php
@@ -8,9 +8,13 @@ include_once "net/Request.php";
 include_once "net/Router.php";
 include_once "io/FileSystemHandler.php";
 
+$cwd = getcwd();
+chdir(dirname(__FILE__));
+
 $settings = new Settings($GLOBALS["settings"]);
 $fileSystemHandler = new FileSystemHandler($settings->dataDirectory);
 $router = new Router($settings, $fileSystemHandler);
 $request = new Request($_SERVER["REQUEST_URI"], $settings, $fileSystemHandler);
 
 $router->route($request);
+chdir($cwd);
