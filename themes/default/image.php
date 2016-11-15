28e609d20ff3350addcdb61a61f4099cebaca167
The Ranger <ranger@risk.ee>
2016-11-15 18:23:01 +0200

    Add navigation buttons

--- themes/default/image.php
+++ themes/default/image.php
@@ -3,18 +3,19 @@
 	<head>
 		<?php UI::PrintHeader() ?>
 		<link rel="stylesheet" type="text/css" href="<?php UI::PrintThemeUrl() ?>/image.css">
+		<link rel="stylesheet" type="text/css" href="<?php UI::PrintThemeUrl() ?>/css/font-awesome.min.css">
 	</head>
 	<body>
 		<aside>
 			<header></header>
-			<nav></nav>
+			<nav><a href=""><i class="fa fa-backward fa-lg" aria-hidden="true"></i></a></nav>
 		</aside>
 
 		<main>
 			<header>
-				<nav>Previous</nav>
+				<nav><a href="" class="nav"><i class="fa fa-arrow-circle-o-left fa-2x" aria-hidden="true"></i></a></nav>
 				<nav><?php UINavigation::PrintBreadcrumb(); ?></nav>
-				<nav>Next</nav>
+				<nav><a href="" class="nav"><i class="fa fa-arrow-circle-o-right fa-2x" aria-hidden="true"></i></a></nav>
 			</header>
 			<article>
 				<img src="<?php echo $_SERVER['PHP_SELF']; ?>" alt="Test image"/>
