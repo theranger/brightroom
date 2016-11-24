58bd2a1f04761c17b69cf582dffbf11944f55d2c
The Ranger <ranger@risk.ee>
2016-11-24 20:29:02 +0200

    Add strict type declaration and fixes

--- lib/io/ImageCache.php
+++ lib/io/ImageCache.php
@@ -15,6 +15,23 @@
  * limitations under the License.
  */
 
+declare(strict_types = 1);
+/**
+ * Copyright 2016 The Ranger <ranger@risk.ee>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 include_once "Folder.php";
 
 class ImageCache extends Folder {
@@ -29,7 +46,7 @@ class ImageCache extends Folder {
 		return $file->read();
 	}
 
-	public function save(string $name, resource $data): bool {
+	public function save(string $name, string $data): bool {
 		$file = new File($this, $name);
 		return $file->save($data);
 	}
