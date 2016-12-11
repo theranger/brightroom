b68b70c9ed5709a5adb41b6f402c77759a13032b
The Ranger <ranger@risk.ee>
2016-12-11 16:38:06 +0200

    Create ACL-enabled FS objects

---
+++ lib/io/SecuredFileSystem.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright 2016 The Ranger <ranger@risk.ee>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+include_once "FileSystem.php";
+include_once "SecuredFolder.php";
+
+/**
+ * Created by The Ranger (ranger@risk.ee) on 2016-12-11
+ *
+ */
+class SecuredFileSystem extends FileSystem {
+
+	private $settings;
+
+	public function __construct(string $base, string $url, Settings $settings) {
+		$this->settings = $settings;
+		parent::__construct($base, $url);
+	}
+
+	protected function initialize(string $base, string $url, string $path) {
+		if (is_dir($path)) {
+			$this->folder = new SecuredFolder($base, $url, $this->settings);
+			$this->root = $this->listFolders($this->folder);
+			$this->entryType = EntryType::FOLDER;
+			return;
+		}
+
+		if (is_file($path)) {
+			$this->folder = new SecuredFolder($base, dirname($url), $this->settings);
+			$this->root = $this->listFolders($this->folder);
+			$this->file = new File($this->folder, basename($url));
+			$this->entryType = EntryType::FILE;
+			return;
+		}
+	}
+
+	private function listFolders(SecuredFolder $folder): SecuredFolder {
+		try {
+			$folders = $this->listFolders($folder->parentSecuredFolder())->getFolders();
+			foreach ($folders as $key => $f) {
+				if (!$f->isEqual($folder)) continue;
+				if ($folder->getURL() != $this->folder->getURL()) return $f;
+				$f->getFolders();
+				return $this->root;
+			}
+
+			return $folder; // This should never happen
+		}
+		catch (IOException $ex) {
+			$this->root = $folder;
+			if ($folder->isEqual($this->folder)) $folder->getFolders();        // Gallery root was requested
+			return $folder;
+		}
+	}
+
+	/**
+	 * @return Folder
+	 * @throws IOException
+	 */
+	public function getSecuredFolder(): SecuredFolder {
+		if (!isset($this->folder)) throw new IOException("Folder does not exist or is not readable");
+
+		return $this->folder;
+	}
+
+	public function getSecuredRoot(): SecuredFolder {
+		return $this->root;
+	}
+}
---
+++ lib/io/SecuredFolder.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright 2016 The Ranger <ranger@risk.ee>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types = 1);
+
+include_once "Folder.php";
+include_once "system/Settings.php";
+include_once "security/User.php";
+include_once "security/Group.php";
+
+/**
+ * Created by The Ranger (ranger@risk.ee) on 2016-12-11
+ *
+ */
+class SecuredFolder extends Folder {
+
+	/**
+	 * @var array|Entity[]
+	 */
+	private $acl = array();
+	private $settings;
+
+	public function __construct(string $base, string $url, Settings $settings) {
+		parent::__construct($base, $url);
+
+		$this->settings = $settings;
+		$accessFile = new File($this, $settings->accessFile);
+
+		if (!$accessFile->open()) {
+			$this->acl = array(new Group(Entity::DEFAULT, Permission::READ));
+			return;
+		}
+
+		while($accessFile->hasNext()) {
+			$entry = $accessFile->readLine();
+			if (empty($entry)) continue;
+
+			$this->acl[] = new User($entry, Permission::READ);
+		}
+
+		$accessFile->close();
+	}
+
+	/**
+	 * @return array|SecuredFolder[]
+	 * @throws SystemException
+	 */
+	public function getFolders(): array {
+		if (!empty($this->children)) return $this->children;
+
+		$dh = opendir($this->path);
+		if ($dh == false) return array();
+		if (!method_exists("Normalizer", "normalize")) throw new SystemException("PHP 'intl' module not installed");
+
+		while (($entry = readdir($dh)) !== false) {
+			if ($entry[0] == '.') continue;
+
+			if (!is_dir($this->path."/".$entry)) continue;
+			$this->children[] = new SecuredFolder($this->base, $this->url."/".Normalizer::normalize($entry), $this->settings);
+		}
+
+		closedir($dh);
+		return $this->children;
+	}
+
+	/**
+	 * @return SecuredFolder
+	 * @throws IOException
+	 */
+	public function parentSecuredFolder(): SecuredFolder {
+		if (empty($this->url) || $this->url == "/") throw new IOException("Parent folder not found");
+		return new SecuredFolder($this->base, dirname($this->url), $this->settings);
+	}
+
+	public function appendACL(Entity $entity) {
+		$this->acl[] = $entity;
+	}
+
+	public function getACL(string $name): Entity {
+		foreach ($this->acl as $entity) {
+			if ($entity->getName() == $name) return $entity;
+		}
+
+		return new Group();
+	}
+}
