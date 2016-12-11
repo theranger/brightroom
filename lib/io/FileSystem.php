7c67f635804329aa74d6c275cffd0fa27a4282d3
The Ranger <ranger@risk.ee>
2016-12-11 16:34:03 +0200

    Make class extendable

--- lib/io/FileSystem.php
+++ lib/io/FileSystem.php
@@ -16,21 +16,6 @@
  */
 
 declare(strict_types = 1);
-/**
- * Copyright 2016 The Ranger <ranger@risk.ee>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 include_once "Folder.php";
 include_once "File.php";
@@ -42,14 +27,16 @@ include_once "EntryType.php";
  */
 class FileSystem {
 
-	private $folder;
-	private $root;
-	private $file;
-	private $entryType = EntryType::UNKNOWN;
+	protected $folder;
+	protected $root;
+	protected $file;
+	protected $entryType = EntryType::UNKNOWN;
 
 	public function __construct(string $base, string $url) {
-		$path = $base."/".trim($url, "/");
+		$this->initialize($base, $url, $base."/".trim($url, "/"));
+	}
 
+	protected function initialize(string $base, string $url, string $path) {
 		if (is_dir($path)) {
 			$this->folder = new Folder($base, $url);
 			$this->root = $this->listFolders($this->folder);
